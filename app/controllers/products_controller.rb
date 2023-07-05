eecd858e5cf6bca2006aefbdb5c6ccf344c66932
Clayton <persinger.clayton@gmail.com>
2023-07-05 09:11:05 -0700

    Update controller so prices are affected by sales

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,10 +2,20 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+    @sale = Sale.where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current)
+    if @sale[0]
+      @products.each do |product|
+        product.price = product.price - product.price * (@sale[0]["percent_off"].to_f / 100)
+      end
+    end
   end
 
   def show
     @product = Product.find params[:id]
-  end
+    @sale = Sale.where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current)
 
+    if @sale[0]
+      @product.price = @product.price - @product.price * (@sale[0]["percent_off"].to_f / 100)
+    end
+  end
 end
